709833cc40eed21fe1271a6f6506ab7f83c3a343
Painkiller3x <116508370+Painkiller3x@users.noreply.github.com>
2024-06-16 16:52:43 +0300

    Create Bredik

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import MainPage from './components/MainPage';
+import MainPage from './components/MainPage/MainPage';
 import AuthPage from './components/AuthPage/AuthPage';
 
 function App() {
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { useNavigate } from 'react-router-dom';
-import NavBar from './NavBar/NavBar';
+import NavBar from '../NavBar/NavBar';
 
 function MainPage() {
     // const navigate = useNavigate();
