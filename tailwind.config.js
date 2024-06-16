55db6d02724383088604aff61498eb45c708cd90
Dimaassik <116507628+Dimaassik@users.noreply.github.com>
2024-06-16 10:48:15 +0000

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    colors:{
+      'violet':{
+      'light':"#a6b3f7",
+      'mid':"#8e59d4",
+      'dark':'#371269'},
+      'light.blue':"#181ec7",
+      'grays':{
+        'light':"#e3e3e3",
+        'mid':"#706f75",
+        'dark':"#333333"},
+      gray: colors.gray,
+      black: colors.black,
+      white: colors.white,
+      },
+    fontFamily:{
+      confortaa:['Comfortaa','sans-serif'],
+      bitter:['Bitter','sans-serif']
+    }
   },
   plugins: [],
 }
-
