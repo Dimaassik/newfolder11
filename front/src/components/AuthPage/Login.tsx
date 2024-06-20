2ec52d6f4634a0fa3f3047656757434d97c153a2
Painkiller3x <116508370+Painkiller3x@users.noreply.github.com>
2024-06-20 15:34:05 +0300

    Add Flexibility to SignUp and Login

--- front/src/components/AuthPage/Login.tsx
+++ front/src/components/AuthPage/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link  } from 'react-router-dom';
 import NavBar from '../elements/NavBar';
 import axios from 'axios';
 import { useUser } from '../UserContext';
@@ -45,25 +45,14 @@ const LoginPage: React.FC = () => {
   return (
     <>
       <NavBar />
-      <section className="flex justify-center mt-2">
-        <div className="authDiv bg-[#e0e0e0]">
-          <h1 className="authText">E-mail</h1>
-          <input
-            id='mail'
-            type="email"
-            className="authInput"
-            placeholder="E-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} />
-          <h1 className="authText">Пароль</h1>
-          <input
-            id="pswd"
-            type="password"
-            className="authInput"
-            placeholder="Пароль"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)} />
-          <button className="button" onClick={handleLogin}>Увійти</button>
+      <section className="flex justify-center mt-2 sm:mt-4 md:mt-6 lg:mt-8">
+        <div className="authDiv bg-[#e0e0e0] p-4 sm:p-6 md:p-8 lg:p-10 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
+          <h1 className="authText text-xl sm:text-3xl md:text-2xl lg:text-xl">E-mail</h1>
+          <input id="mail" type="email" className="authInput w-full mb-2 p-2 sm:p-3 md:p-4" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <h1 className="authText text-xl sm:text-3xl md:text-2xl lg:text-xl">Пароль</h1>
+          <input id="pswd" type="password" className="authInput w-full mb-2 p-2 sm:p-3 md:p-4" placeholder="Пароль" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <button className="button w-full py-2 sm:py-3 md:py-4 lg:py-5" onClick={handleLogin}>Увійти</button>
+          <Link to="/signup" className="button w-full text-center mt-2 py-2 sm:py-3 md:py-4 lg:py-5">Створити аккаунт</Link>
         </div>
       </section>
     </>
--- front/src/components/AuthPage/SignUp.tsx
+++ front/src/components/AuthPage/SignUp.tsx
@@ -50,18 +50,20 @@ const SignUpPage: React.FC = () => {
   return (
     <>
       <NavBar />
-      <section className="flex justify-center mt-2">
-        <div className="authDiv bg-[#e0e0e0]">
-          <h1 className="authText">Ім'я</h1>
-          <input type="text" className="authInput" placeholder="Ім'я" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-          <h1 className="authText">E-mail</h1>
-          <input id="mail" type="email" className="authInput" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <h1 className="authText">Пароль</h1>
-          <input type="password" className="authInput" placeholder="Пароль" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <h1 className="authText">Повторіть пароль</h1>
-          <input type="password" className="authInput mb-2" placeholder="Повторіть пароль" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-          <button className="button" onClick={handleSignUp}>Зареєструватися</button>
-          <Link to="/login" className="button text-center">Маю аккаунт</Link>
+      <section className="flex justify-center mt-2 sm:mt-4 md:mt-6 lg:mt-8">
+        <div className="authDiv bg-[#e0e0e0] p-4 sm:p-6 md:p-8 lg:p-10 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
+          <h1 className="authText sm:text-3xl md:text-2xl lg:text-xl">Ім'я</h1>
+          <input type="text" className="authInput w-full mb-2 p-2 sm:p-3 md:p-4" placeholder="Ім'я" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+          <h1 className="authText sm:text-3xl md:text-2xl lg:text-xl">E-mail</h1>
+          <input id="mail" type="email" className="authInput w-full mb-2 p-2 sm:p-3 md:p-4" placeholder="E-mail" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <h1 className="authText sm:text-3xl md:text-2xl lg:text-xl">Пароль</h1>
+          <input type="password" className="authInput w-full mb-2 p-2 sm:p-3 md:p-4" placeholder="Пароль" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <h1 className="authText sm:text-3xl md:text-2xl lg:text-xl">Повторіть пароль</h1>
+          <input type="password" className="authInput w-full mb-2 p-2 sm:p-3 md:p-4" placeholder="Повторіть пароль" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+          <button className="button w-full py-2 sm:py-3 md:py-4 lg:py-5" onClick={handleSignUp}>
+            Зареєструватися
+          </button>
+          <Link to="/login" className="button w-full text-center mt-2 py-2 sm:py-3 md:py-4 lg:py-5">Маю аккаунт</Link>
         </div>
       </section>
     </>
