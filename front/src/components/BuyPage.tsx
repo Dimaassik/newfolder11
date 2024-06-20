063cd84e6ab4b97e92075ad10a6f4143becf6362
Dimaassik <116507628+Dimaassik@users.noreply.github.com>
2024-06-20 17:19:48 +0000

    buypage 1.1

--- front/src/components/BuyPage.tsx
+++ front/src/components/BuyPage.tsx
@@ -28,24 +28,24 @@ const BuyPage: React.FC = () => {
         <>
             <NavBar />
             <section className='divrow'>
-                <div className='mt-2 divrow border-2 flex-col p-4 border-black gap-1'>
+                <div className='my-2 divrow border-2 flex-col p-4 border-black gap-1'>
                     {cart.length > 0 ? (
                 <>
                     {cart.map((item : any) => (
                     <div className='flex justify-between border-2 w-full border-gray-600 rounded-xl bg-gray-100 py-6 px-4 shadow-xl' >
                         <img className='w-[15rem] h-[15rem] object-contain' src={item.img} alt='product' />
-                        <div className="flex flex-col">
+                        <div className="mx-2 flex flex-col justify-center w-[15rem]">
+                            <button className='button' onClick={() => RemFromCart(item)}>-</button>
                             <h1 className='text-lg font-bold'>{item.name}</h1>
                             <p>{item.count}</p>
                             <h3 className='font-semibold text-gray-500'>${item.price}</h3>
-                            <button className='button' onClick={() => RemFromCart(item)}>-</button>
                             <button className='button' onClick={() => AddToCart(item)}>+</button>
                         </div>
                     </div>
                 ))}
                 <div className='divrow justify-between w-full'>
-                    <h1 className='font-bitter ml-2 text-black sm:text-3xl md:text-2xl lg:text-xl'>Всього</h1>
-                    <h1 className='font-bitter ml-2 text-black sm:text-3xl md:text-2xl lg:text-xl' >${TotalPrice}</h1>
+                    <h1 className='font-bitter ml-2 text-black text-4xl'>Всього</h1>
+                    <h1 className='font-bitter ml-2 text-black text-4xl' >${TotalPrice}</h1>
                 </div>
                 </>
                 ):(
