08eb573eab4c54b531908f4765a6d89fc65ef643
Dimaassik <116507628+Dimaassik@users.noreply.github.com>
2024-06-20 17:44:22 +0000

    id update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const jsonServer = require('json-server');
 const path = require('path');
+const uuid = require('uuid');
 
 const app = express();
 const PORT = 3001;
@@ -10,30 +11,8 @@ const PORT = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
+
 const jsonServerRouter = jsonServer.router(path.join(__dirname, 'db.json'));
-app.get('/category/:category/:productName', (req, res) => {
-  const { category, productName } = req.params;
-
-  let products = [];
-  if (category === 'keyboards') {
-    products = jsonServerRouter.db.get('keyboards').value();
-  } else if (category === 'mice') {
-    products = jsonServerRouter.db.get('mice').value();
-  }else if (category === 'monitors') {
-    product = findProduct(monitors, productName);
-}
-
-  const product = products.find(p => {
-    const formattedTitle = p.title.replace(/\s+/g, '-').toLowerCase();
-    return formattedTitle === productName;
-  });
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: 'Product not found' });
-  }
-});
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
@@ -48,6 +27,7 @@ app.post('/login', (req, res) => {
 
 app.post('/signup', (req, res) => {
   const { firstName, email, password } = req.body;
+  const userId = uuid.v4();
 
   const existingUser = jsonServerRouter.db.get('users').find({ email }).value();
   if (existingUser) {
@@ -55,7 +35,7 @@ app.post('/signup', (req, res) => {
     return;
   }
 
-  const newUser = { firstName, email, password };
+  const newUser = { firstName, email, password, userId };
   jsonServerRouter.db.get('users').push(newUser).write();
 
   res.status(201).json(newUser);
