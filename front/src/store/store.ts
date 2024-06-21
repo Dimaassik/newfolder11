034910eaf58e1bdd108c8367bfcaf8bf4b0b82a7
Dimaassik <116507628+Dimaassik@users.noreply.github.com>
2024-06-21 08:45:38 +0000

    Your branch is up to date with 'origin/main'.

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import buycartSlice from "./buycart";
+import thunk from 'redux-thunk';
 
 export const store = configureStore({
     reducer:{
         cart: buycartSlice
-    }
+    },
+    
  })
